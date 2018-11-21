95606e34874e2a39c614e3181d696200ecd20534
Hiroshi Fujiwara <fujiwara@no1s.biz>
2018-11-21 19:52:04 +0900

    chapter02 completed

--- chapter02/sample05.py
+++ chapter02/sample05.py
@@ -1,11 +1,13 @@
-num1 = 1
+a = 10
 
-num1 = num1 + 1
-print(num1) #2
+a += 2
+print(a) # 12
 
-num2 = 5
-num2 += 1
-print(num2) #6
+a -= 2
+print(a) # 10
 
-num2 -= 1
-print(num2) #5
+a *= 2
+print(a) # 20
+
+a /= 2
+print(a) # 10.0
