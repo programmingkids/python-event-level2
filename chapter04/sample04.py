67807327f14e5b98141d75c9f15af861b0e9b650
Hiroshi Fujiwara <fujiwara@no1s.biz>
2018-11-22 18:02:13 +0900

    chapter04 completed

--- chapter04/sample04.py
+++ chapter04/sample04.py
@@ -15,7 +15,7 @@ canvas = Canvas(root, width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
 canvas.pack()
 
 canvas.create_rectangle(0,0,BOX_SIZE,BOX_SIZE, fill="red")
-canvas.create_rectangle(BOX_SIZE,0,BOX_SIZE*2,BOX_SIZE, fill="red")
+canvas.create_rectangle(BOX_SIZE,0,BOX_SIZE*2,BOX_SIZE, fill="blue")
 canvas.create_rectangle(BOX_SIZE,BOX_SIZE,BOX_SIZE*2,BOX_SIZE*2, fill="green")
 
 root.mainloop()
